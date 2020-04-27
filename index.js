300953711064ed3c7a0674cb71a18970aa68c552
Max Zavaleta Villar <Max.Zavaleta@oechsle.pe>
2020-04-26 23:51:49 -0500

    Web api completed!!!

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
 const app=express()
+//midleware
+app.use(express.json());
 
 //routes
 app.use(require('./routes/index'))
 app.use('/song/api/v1',require('./routes/song'))
+app.use('/user/api/v1',require('./routes/users'))
+
 
 app.listen(3000,function(){
     console.log('Server ready')
--- routes/song.js
+++ routes/song.js
@@ -2,9 +2,50 @@ const { Router} = require('express')
 
 const router = Router();
 const songs = require('../db/songs.json')
-
+var mid = songs.length;
 router.get("/",function(req,res){
     res.json(songs)
 })
 
+router.post("/",function(req,res){
+    const {name,singer,year,genre} = req.body;
+    if(name&&singer&&year&&genre){
+        mid = mid+1;
+        const id=mid;
+        const song={id, ...req.body}
+        songs.push(song);
+        res.json(song)
+    }else{
+        res.status(500).end('Error in request')
+    }
+})
+
+router.get("/:id",function(req,res){
+    const {id} = req.params
+    if (id>mid || id<1){
+        res.status(404).end('Song not found!!')
+    }else{
+        songs.forEach(function(song,i){
+            if(song.id==id){
+                return res.json(song)                
+            }
+        })
+        res.status(404).end('Song not found!!')
+    }
+})
+
+router.delete('/:id',function(req, res){
+    const { id } = req.params
+    if (id>mid || id<1){
+        res.status(404).end('Song not found!!')
+    }else{
+        songs.forEach(function(song,i){
+            if(song.id==id){
+                songs.splice(i,1)
+                res.end('Song deleted!!!')                                
+            }
+            res.status(404).end('Song not found!!')
+        })
+    }
+})
 module.exports=router
